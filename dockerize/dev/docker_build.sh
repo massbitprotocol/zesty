565a37f97b451aafee8c4001c38c84d140f07004
anhminhbo <wormscott12397@gmail.com>
2023-03-16 11:34:37 +0700

    chore: update dockerize dev

--- dockerize/dev/docker_build.sh
+++ dockerize/dev/docker_build.sh
@@ -1,3 +1,11 @@
-docker build -f ./Dockerfile -t massbit/massbitroute_zesty:v0.0.1 ../..
+# Specify env
+ENV=dev
+
+# Get the version
+ZESTY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep ZESTY | cut -d"=" -f2)
+JUICY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep JUICY | cut -d"=" -f2)
+
+# Build docker images
+docker build -f ./Dockerfile -t massbit/massbitroute_zesty:${ZESTY_TAG} --build-arg JUICY_TAG=${JUICY_TAG} --build-arg ENV=${ENV} ../..
 
 # docker-compose up -d --force-recreate
