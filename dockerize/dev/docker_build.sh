cdfbd1f20ba0d0ec32a041fba4025e43ca7552cf
anhminhbo <wormscott12397@gmail.com>
2023-03-17 15:26:21 +0700

    chore: create new tag

--- dockerize/dev/docker_build.sh
+++ dockerize/dev/docker_build.sh
@@ -1,11 +1,22 @@
 # Specify env
 ENV=dev
 
-# Get the version
-ZESTY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep ZESTY | cut -d"=" -f2)
+# Get the version of ZESTY from tag
+ZESTY_TAG=$(git ls-remote --tags --sort='v:refname' git@github.com:massbitprotocol/zesty.git| tail -n1 | cut -d/ -f3)
+# ZESTY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep ZESTY | cut -d"=" -f2)
+
+# Get the version of JUICY based on version from ZESTY
 JUICY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep JUICY | cut -d"=" -f2)
+
+# Get latest tag
+git fetch --all --tags --force
+
+# Checkout latest tag
+git checkout tags/${ZESTY_TAG}
 
 # Build docker images
 docker build -f ./Dockerfile -t massbit/massbitroute_zesty:${ZESTY_TAG} --build-arg JUICY_TAG=${JUICY_TAG} --build-arg ENV=${ENV} ../..
 
+# Export ZESTY TAG for next script
+export ZESTY_TAG
 # docker-compose up -d --force-recreate
