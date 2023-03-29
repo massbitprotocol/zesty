94c9b4d34961eade29d2519776ada6bfac4fa417
hoanito <hhoang219@gmail.com>
2023-03-29 15:05:52 +0700

    Add supervisor config

--- script/update-zesty.sh
+++ script/update-zesty.sh
@@ -5,24 +5,20 @@ echo "Zesty last update at $(date)" > /usr/local/openresty/zesty-update.log
 
 set -e
 rm -rf /tmp/zesty
-mkdir /tmp/zesty
 git clone --quiet https://github.com/massbitprotocol/zesty.git -b $1 /tmp/zesty
 
+cp  /tmp/zesty/nginx/conf/nginx.conf  /usr/local/openresty/nginx/conf/nginx.conf
+cp -r /tmp/zesty/nginx/conf/include /usr/local/openresty/nginx/conf/extensions/
+cp -r /tmp/zesty/nginx/conf/subconf /usr/local/openresty/nginx/conf/extensions/
+cp -r /tmp/zesty/nginx/modules/* /usr/local/openresty/nginx/modules/extensions/
+cp -r /tmp/zesty/nginx/modules/* /usr/local/openresty/nginx/extensions/
 
-cp -r /tmp/zesty/volume/bin/openresty /usr/local/
 
-cp -r /tmp/zesty/volume/bin/openresty/nginx/sbin/nginx /usr/bin/
-cp -r /tmp/zesty/volume/nginx.conf   /usr/local/openresty/nginx/conf/nginx.conf
-cp -r /tmp/zesty/volume/modules.conf   /usr/local/openresty/nginx/conf/modules.conf
-cp -r /tmp/zesty/volume/data   /usr/local/openresty/nginx/data
-cp -r /tmp/zesty/volume/modules/*   /usr/local/openresty/nginx/extensions
-chmod 755 /usr/local/openresty/nginx/data/vts_gw.db
+cp -r /tmp/zesty/nginx/luascripts/* /usr/local/openresty/lualib/mbr/
 
-cp -r /tmp/zesty/volume/conf   /usr/local/openresty/nginx/conf/include
-cp -r /tmp/zesty/volume/conf/subconf   /usr/local/openresty/nginx/conf/subconf
+# load supervisor config and start
+cp -r /tmp/zesty/supervisord/openresty.conf   /etc/supervisor/conf.d/openresty.conf
 
-cp -r /tmp/zesty/volume/mbr/ssl   /etc/gateway/
-cp -r /tmp/zesty/volume/mbr/ssl   /.mbr/
-cp -r /tmp/zesty/volume/mbr/util /.mbr/
+
 
 /usr/bin/nginx -s reload
