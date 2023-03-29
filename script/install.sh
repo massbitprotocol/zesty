dd968bb526ff44154cb66174d1b2d881ea564e19
anhminhbo <wormscott12397@gmail.com>
2023-03-29 11:21:30 +0700

    chore: delete prometheus, supervisor folder at root, add symbolic link for mbr bin in install script

--- script/install.sh
+++ script/install.sh
@@ -81,7 +81,7 @@ export MBR_CONFIG_FILE=/.mbr/env.yaml
 # Load and run CLI
 wget -q https://public-massbit.s3.ap-southeast-1.amazonaws.com/binary/mbr-$juicy_version -O /.mbr/mbr
 chmod +x  /.mbr/mbr
-ln -s /.mbr/executable/mbr /usr/bin/mbr
+ln -s /.mbr/mbr /usr/bin/mbr
 mbr login
 
 mbr gateway init
