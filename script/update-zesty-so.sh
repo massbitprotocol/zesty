bf617b8852eefec5442fd929920d8c9c9505cbe9
hoanito <hhoang219@gmail.com>
2023-04-24 15:40:18 +0700

    fix zesty binary update

--- script/update-zesty-so.sh
+++ script/update-zesty-so.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 update_zesty (){
-    wget -q https://public-massbit.s3.ap-southeast-1.amazonaws.com/so-zesty/go-build/zesty-$1.so -O /usr/local/openresty/nginx/modules/extensions/zesty-$1.so
+    wget -q https://public-massbit.s3.ap-southeast-1.amazonaws.com/so-zesty/go-build/zesty-$1 -O /.mbr/zesty
     export LD_LIBRARY_PATH=/usr/local/openresty/nginx/modules/extensions
     echo $1 > /.mbr/zesty.ver
     nginx -s reload
     mkdir -p /var/run/nginx-client-body
 
-    echo "$(date) - Zesty.so updated successfully - $1"
+    echo "$(date) - Zesty binary updated successfully - $1"
     exit 0
 }
 
@@ -17,6 +17,6 @@ folder_path="/.mbr/zesty.ver"
 if [ ! -f $folder_path ] || [ $(cat $folder_path) != $1 ]; then
     update_zesty $1
 else 
-    echo "$(date) - Zesty.so is up to date - $1"
+    echo "$(date) - Zesty binary is up to date - $1"
     exit 0
 fi
