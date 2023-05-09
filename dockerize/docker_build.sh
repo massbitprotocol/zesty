2f9ef345c54bafbe12c00c17172bbdd38ebdaa80
Hoanito <hhoang219@gmail.com>
2023-05-09 14:55:39 +0700

    move version file to new git (#40) (#41)

--- dockerize/docker_build.sh
+++ dockerize/docker_build.sh
@@ -6,16 +6,16 @@ git ls-remote --tags --sort='v:refname' git@github.com:massbitprotocol/zesty.git
 
 # Get the version of ZESTY from tag
 ZESTY_TAG=$(cat ZESTY_TAG)
-# ZESTY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep ZESTY | cut -d"=" -f2)
+# ZESTY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/version/release/version | grep ZESTY | cut -d"=" -f2)
 
 # Get the version of JUICY based on version from ZESTY
-JUICY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep JUICY | cut -d"=" -f2)
+JUICY_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/version/release/version | grep JUICY | cut -d"=" -f2)
 
 # Get the version of JUICY based on version from ZESTY
-ZESTY_SO_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep ZESTY_SO | cut -d"=" -f2)
+ZESTY_SO_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/version/release/version | grep ZESTY_SO | cut -d"=" -f2)
 
 # Get the version of JUICY based on version from ZESTY
-ZESTY_NGINX_SO_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/zesty/release/version | grep ZESTY_NGINX_SO | cut -d"=" -f2)
+ZESTY_NGINX_SO_TAG=$(curl -q https://raw.githubusercontent.com/massbitprotocol/version/release/version | grep ZESTY_NGINX_SO | cut -d"=" -f2)
 
 # Get latest tag
 git fetch --all --tags --force
